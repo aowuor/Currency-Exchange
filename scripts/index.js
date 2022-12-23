76fffca62bf063fd4a560a059f0f4f827d6a80ce
aowuor <angelaowuor@gmail.com>
2022-12-24 00:17:54 +0300

    updated REST API

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,58 @@
-
+let obje;
 // Get data from API
-customFetch("https://open.er-api.com/v6/latest/USD", "GET")
+function getData(){
+    let originalRate = document.getElementById('original')
+    let revisedRate = document.getElementById('revised')
+    
+    originalRate.addEventListener("click", ()=> {
+        customFetch("https://open.er-api.com/v6/latest/USD", "GET")
+    })
+    revisedRate.addEventListener("click", ()=> {
+        customFetch("http://localhost:3000/exchangeRates", "GET")
+
+    })
+}
+getData()
+
 
 // Display Exchange Rates
 function renderExchangeRate(rates){
     let date = document.getElementById('date')
-    date.innerText = rates.time_last_update_utc
+    let ul = document.getElementById('rates_list')
 
-    console.log(rates.rates)
-    for(let rate in rates.rates){
-        let ul = document.getElementById('rates_list')
-        let li = document.createElement('li')
-        li.setAttribute("class","list-group-item")
-        ul.appendChild(li)
-        li.innerText = `${rate} - ${rates.rates[rate]}`
+    for(let item in rates){
+        date.innerText = rates.time_last_update_utc
+        for(let currency in rates.rates){
+            let li = document.createElement('li')
+            li.setAttribute("class","list-group-item")
+            ul.appendChild(li)
+            li.innerText = `${currency} - ${rates.rates[currency]}`
+        }   
     }
 }
+       
 
-//Display exchange rate select options
+//Display exchange-rate select options
 function populateFromCurrencyOptions(rates){
+    let fromSelect = document.getElementById('from')
     for(let currency in rates.rates){
         let option = document.createElement('option')
-        let fromSelect = document.getElementById('from')
         fromSelect.appendChild(option)
         option.text = currency
     }
-
 }
 
 function populateToCurrencyOptions(rates){
+    let toSelect = document.getElementById('to')
     for(let currency in rates.rates){
         let option = document.createElement('option')
-        let toSelect = document.getElementById('to')
         toSelect.appendChild(option)
         option.text = currency
     }
-    
 }
+    
 
-//Calculates exchange rate for a specific amount based on currencies selected by user
+// //Calculates exchange rate for a specific amount based on currencies selected by user
 function calculateEquivalentAmount(rates){
     let form = document.getElementById('api_rate')
     form.addEventListener("submit", (e) => {
@@ -50,11 +64,31 @@ function calculateEquivalentAmount(rates){
         let toOption = to.options[to.selectedIndex].text
         let amount = document.getElementById('amount')
         amount.innerText = form.amount.value
-
-        let calculatedAmount = (form.amount.value * rates.rates[toOption])/rates.rates[fromOption]
-   
+        
+        for(let currency in rates.rates){
+            let calculatedAmount = (form.amount.value * rates.rates[toOption])/rates.rates[fromOption]
+            let result = document.getElementById('result')
+            result.innerText = calculatedAmount.toFixed(2)
+        }
     })   
 }
+
+
+function postToDatabase(rates){
+    let revisedRate = document.getElementById('revised')
+    revisedRate.addEventListener("click", ()=> {
+        // customFetch("https://open.er-api.com/v6/latest/USD", "GET")
+        obje = {
+            time_last_update_utc: rates.time_last_update_utc,
+            time_next_update_utc:rates.time_next_update_utc,
+            time_eol_unix: rates.time_eol_unix,
+            base_code: rates.base_code,
+            rates: rates.rates
+        } 
+    })
+    customFetch("http://localhost:3000/exchangeRates", "POST", obje)
+}
+
 
 // custom GET, POST, PATCH and DELETE Fetch functions
 function customFetch(url,type,data){
@@ -68,7 +102,8 @@ function customFetch(url,type,data){
             populateFromCurrencyOptions(data)
             populateToCurrencyOptions(data)
             calculateEquivalentAmount(data)
-            // console.log(data.rates);
+            postToDatabase(data)
+        
         })
         .catch((error) => console.log(error))
     }
@@ -79,7 +114,7 @@ function customFetch(url,type,data){
                 "Content-type": "application/json",
                 Accept: "application/json",
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(obje)
         })
         .then((res)=> res.json())
         .then((data) => console.log(data))
@@ -108,3 +143,67 @@ function customFetch(url,type,data){
         .catch((error) => console.log(error))
     }
 }
+
+
+
+
+
+
+// // Display Exchange Rates
+// function renderExchangeRate(rates){
+//     obje = rates
+//     console.log(rates)
+//     let date = document.getElementById('date')
+//     date.innerText = rates.time_last_update_utc
+
+//     // console.log(rates.rates)
+//     for(let rate in rates.rates){
+//         let ul = document.getElementById('rates_list')
+//         let li = document.createElement('li')
+//         li.setAttribute("class","list-group-item")
+//         ul.appendChild(li)
+//         li.innerText = `${rate} - ${rates.rates[rate]}`
+//     }
+// }
+
+// // //Display exchange-rate select options
+// // function populateFromCurrencyOptions(rates){
+    
+// //     for(let currency in rates.rates){
+// //         let option = document.createElement('option')
+// //         let fromSelect = document.getElementById('from')
+// //         fromSelect.appendChild(option)
+// //         option.text = currency
+// //     }
+
+// // }
+
+// // function populateToCurrencyOptions(rates){
+// //     for(let currency in rates.rates){
+// //         let option = document.createElement('option')
+// //         let toSelect = document.getElementById('to')
+// //         toSelect.appendChild(option)
+// //         option.text = currency
+// //     }
+    
+// // }
+
+// // //Calculates exchange rate for a specific amount based on currencies selected by user
+// // function calculateEquivalentAmount(rates){
+// //     let form = document.getElementById('api_rate')
+// //     form.addEventListener("submit", (e) => {
+// //         e.preventDefault()
+
+// //         let from = document.getElementById('from')
+// //         let fromOption = from.options[from.selectedIndex].text
+// //         let to = document.getElementById('to')
+// //         let toOption = to.options[to.selectedIndex].text
+// //         let amount = document.getElementById('amount')
+// //         amount.innerText = form.amount.value
+
+// //         let calculatedAmount = (form.amount.value * rates.rates[toOption])/rates.rates[fromOption]
+// //         let result = document.getElementById('result')
+// //         result.innerText = calculatedAmount.toFixed(2)
+   
+// //     })   
+// // }
