46877472cc8003430ca8f5566584a82d7565e22d
aowuor <angelaowuor@gmail.com>
2022-12-28 01:00:27 +0300

    added patch and post requests

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,30 @@
 let obje;
-// Get data from API
-function getData(){
-    let originalRate = document.getElementById('original')
-    let revisedRate = document.getElementById('revised')
-    
-    originalRate.addEventListener("click", ()=> {
-        customFetch("https://open.er-api.com/v6/latest/USD", "GET")
-    })
-    revisedRate.addEventListener("click", ()=> {
-        customFetch("http://localhost:3000/exchangeRates", "GET")
+let date = new Date().getDate()
+let firstTime = true;
+let originalRate = document.getElementById('original')
+let revisedRate = document.getElementById('revised')
 
-    })
+if(firstTime){
+    customFetch("https://open.er-api.com/v6/latest/USD", "GET")
+    firstTime = false
 }
-getData()
 
+originalRate.addEventListener("click", getAPIData)
+revisedRate.addEventListener("click", getDatabaseData)
 
 // Display Exchange Rates
 function renderExchangeRate(rates){
     let date = document.getElementById('date')
     let ul = document.getElementById('rates_list')
-
-    for(let item in rates){
-        date.innerText = rates.time_last_update_utc
-        for(let currency in rates.rates){
-            let li = document.createElement('li')
-            li.setAttribute("class","list-group-item")
-            ul.appendChild(li)
-            li.innerText = `${currency} - ${rates.rates[currency]}`
-        }   
+    date.innerText = `Last update: ${rates.time_last_update_utc}`
+    for(let currency in rates.rates){
+        let li = document.createElement('li')
+        li.setAttribute("class","list-group-item")
+        ul.appendChild(li)
+        li.innerText = `${currency} - ${rates.rates[currency]}`     
     }
 }
        
-
 //Display exchange-rate select options
 function populateFromCurrencyOptions(rates){
     let fromSelect = document.getElementById('from')
@@ -64,31 +57,54 @@ function calculateEquivalentAmount(rates){
         let toOption = to.options[to.selectedIndex].text
         let amount = document.getElementById('amount')
         amount.innerText = form.amount.value
-        
-        for(let currency in rates.rates){
-            let calculatedAmount = (form.amount.value * rates.rates[toOption])/rates.rates[fromOption]
-            let result = document.getElementById('result')
-            result.innerText = calculatedAmount.toFixed(2)
-        }
+
+        let calculatedAmount = (form.amount.value * rates.rates[toOption])/rates.rates[fromOption]
+        let result = document.getElementById('result')
+        result.innerText = calculatedAmount.toFixed(2)
     })   
 }
 
-
-function postToDatabase(rates){
-    let revisedRate = document.getElementById('revised')
-    revisedRate.addEventListener("click", ()=> {
-        // customFetch("https://open.er-api.com/v6/latest/USD", "GET")
-        obje = {
-            time_last_update_utc: rates.time_last_update_utc,
-            time_next_update_utc:rates.time_next_update_utc,
-            time_eol_unix: rates.time_eol_unix,
-            base_code: rates.base_code,
-            rates: rates.rates
-        } 
-    })
-    customFetch("http://localhost:3000/exchangeRates", "POST", obje)
+function createObject(rates){
+    obje = {
+        time_last_update_utc: rates.time_last_update_utc,
+        time_next_update_utc:rates.time_next_update_utc,
+        time_eol_unix: rates.time_eol_unix,
+        base_code: rates.base_code,
+        rates: rates.rates
+    }
+    // console.log(obje.time_last_update_utc)
+    // console.log(rates.time_last_update_utc)
 }
 
+function postToDatabase(rates){
+    if(rates.time_last_update_utc.split(" ")[1] != date){
+        customFetch("http://localhost:3000/exchangeRates", "POST", obje)
+    }
+    let originalRate = document.getElementById('original')
+    originalRate.addEventListener("click", () => {
+        createObject
+        customFetch("http://localhost:3000/exchangeRates", "POST", obje)
+    })
+
+}
+
+function updateDatabase(){
+    let updateForm = document.getElementById('updateForm')
+    updateForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        obje.rates[updateForm.currencyInput.value] = `${updateForm.rateInput.value}`
+        customFetch(`http://localhost:3000/exchangeRates`, "PATCH", obje)  
+    })
+}
+updateDatabase()
+
+function getAPIData(){
+    customFetch("https://open.er-api.com/v6/latest/USD", "GET")  
+}
+
+function getDatabaseData(){
+    customFetch("http://localhost:3000/exchangeRates", "GET")
+}
 
 // custom GET, POST, PATCH and DELETE Fetch functions
 function customFetch(url,type,data){
@@ -102,8 +118,8 @@ function customFetch(url,type,data){
             populateFromCurrencyOptions(data)
             populateToCurrencyOptions(data)
             calculateEquivalentAmount(data)
+            createObject(data)
             postToDatabase(data)
-        
         })
         .catch((error) => console.log(error))
     }
@@ -144,66 +160,21 @@ function customFetch(url,type,data){
     }
 }
 
+// function changeRate(rates){
+//     let updateForm = document.getElementById('updateForm')
+//     updateForm.addEventListener("submit", (e) => {
+//         e.preventDefault()
+//         // console.log(`${updateForm.currencyInput.value} : ${updateForm.rateInput.value} `)
+//         console.log(rates)
+//         for(let key in rates){
+//             // console.log(rt)
+//         }
+        
+//     })
+//     document.getElementById('change').addEventListener("click", () => {
+//         // console.log(`${updateForm.currencyInput.value} : ${updateForm.rateInput.value} `)
+        
+//     })
 
-
-
-
-
-// // Display Exchange Rates
-// function renderExchangeRate(rates){
-//     obje = rates
-//     console.log(rates)
-//     let date = document.getElementById('date')
-//     date.innerText = rates.time_last_update_utc
-
-//     // console.log(rates.rates)
-//     for(let rate in rates.rates){
-//         let ul = document.getElementById('rates_list')
-//         let li = document.createElement('li')
-//         li.setAttribute("class","list-group-item")
-//         ul.appendChild(li)
-//         li.innerText = `${rate} - ${rates.rates[rate]}`
-//     }
 // }
-
-// // //Display exchange-rate select options
-// // function populateFromCurrencyOptions(rates){
-    
-// //     for(let currency in rates.rates){
-// //         let option = document.createElement('option')
-// //         let fromSelect = document.getElementById('from')
-// //         fromSelect.appendChild(option)
-// //         option.text = currency
-// //     }
-
-// // }
-
-// // function populateToCurrencyOptions(rates){
-// //     for(let currency in rates.rates){
-// //         let option = document.createElement('option')
-// //         let toSelect = document.getElementById('to')
-// //         toSelect.appendChild(option)
-// //         option.text = currency
-// //     }
-    
-// // }
-
-// // //Calculates exchange rate for a specific amount based on currencies selected by user
-// // function calculateEquivalentAmount(rates){
-// //     let form = document.getElementById('api_rate')
-// //     form.addEventListener("submit", (e) => {
-// //         e.preventDefault()
-
-// //         let from = document.getElementById('from')
-// //         let fromOption = from.options[from.selectedIndex].text
-// //         let to = document.getElementById('to')
-// //         let toOption = to.options[to.selectedIndex].text
-// //         let amount = document.getElementById('amount')
-// //         amount.innerText = form.amount.value
-
-// //         let calculatedAmount = (form.amount.value * rates.rates[toOption])/rates.rates[fromOption]
-// //         let result = document.getElementById('result')
-// //         result.innerText = calculatedAmount.toFixed(2)
-   
-// //     })   
-// // }
+// changeRate()
